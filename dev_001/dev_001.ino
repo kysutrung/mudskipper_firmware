39d12ad6d489312be3ae71068116f4af6e309d1f
trung_pc_l <trungnd2767@gmail.com>
2024-10-29 16:03:57 +0700

    pip boy ui look bad so i create the new one

--- dev_001/dev_001.ino
+++ dev_001/dev_001.ino
@@ -6,110 +6,55 @@ int mainMenuMode = 0;
 
 //FUNCTIONS
 
-void setMainMenu(){
-  setQuikMode();
-  delay(2000);
-  setProgramsMenu();
-  delay(2000);
-  setGeneralSettingMode();
-  delay(2000);
-  setAboutDisplay();
-  delay(2000);
-  setQuikMode();
-  delay(2000);
-  setProgramsMenu();
-  delay(2000);
-  setGeneralSettingMode();
-  delay(2000);
-  setAboutDisplay();
+void getButtonz(){
+  
+}
+
+void setMainMenu(int num_a){
+  StickCP2.Display.clear();
+  StickCP2.Display.setTextSize(1);
+  StickCP2.Display.setCursor(40, 15);
+  StickCP2.Display.printf("QUICK ACCESS");
+  StickCP2.Display.setCursor(40, 45);
+  StickCP2.Display.printf("PROGRAMS");
+  StickCP2.Display.setCursor(40, 75);
+  StickCP2.Display.printf("GENERAL SETING");
+  StickCP2.Display.setCursor(40, 105);
+  StickCP2.Display.printf("ABOUT");
+  
+  int y_dot = 0;
+  switch (num_a) {
+  case 1:
+    y_dot = 15;
+    break;
+  case 2:
+    y_dot = 45;
+    break;
+  case 3:
+    y_dot = 75;
+    break;
+  case 4:
+    y_dot = 105;
+    break;
+  }
+  StickCP2.Display.fillRect(12, y_dot, 15, 15, WHITE);
+
 }
 
 void setQuikMode(){
-  StickCP2.Display.setTextSize(1);
-  StickCP2.Display.clear();
 
-  StickCP2.Display.drawFastHLine(0, 17, 240, GREEN);
-  StickCP2.Display.drawRect(23, 8, 40, 10, GREEN);
-  StickCP2.Display.fillRect(26, 8, 34, 8, BLACK);
-  StickCP2.Display.fillRect(24, 9, 38, 10, BLACK);
-
-  StickCP2.Display.setCursor(31, 6);
-  StickCP2.Display.printf("QUIK");
-  StickCP2.Display.setCursor(79, 6);
-  StickCP2.Display.printf("PROG");
-  StickCP2.Display.setCursor(127, 6);
-  StickCP2.Display.printf("GSET");
-  StickCP2.Display.setCursor(177, 6);
-  StickCP2.Display.printf("ABOUT");
-
-  StickCP2.Display.setCursor(35, 65);
-  StickCP2.Display.printf("Press M5 to enter this mode");
 }
 
 void setProgramsMenu(){
-  StickCP2.Display.setTextSize(1);
-  StickCP2.Display.clear();
 
-  StickCP2.Display.drawFastHLine(0, 17, 240, GREEN);
-  StickCP2.Display.drawRect(70, 8, 40, 10, GREEN);
-  StickCP2.Display.fillRect(73, 8, 34, 8, BLACK);
-  StickCP2.Display.fillRect(71, 9, 38, 10, BLACK);
-
-  StickCP2.Display.setCursor(31, 6);
-  StickCP2.Display.printf("QUIK");
-  StickCP2.Display.setCursor(79, 6);
-  StickCP2.Display.printf("PROG");
-  StickCP2.Display.setCursor(127, 6);
-  StickCP2.Display.printf("GSET");
-  StickCP2.Display.setCursor(177, 6);
-  StickCP2.Display.printf("ABOUT");
-
-  StickCP2.Display.setCursor(35, 65);
-  StickCP2.Display.printf("Press M5 to enter this mode");
 }
 
 void setGeneralSettingMode(){
-  StickCP2.Display.setTextSize(1);
-  StickCP2.Display.clear();
 
-  StickCP2.Display.drawFastHLine(0, 17, 240, GREEN);
-  StickCP2.Display.drawRect(118, 8, 40, 10, GREEN);
-  StickCP2.Display.fillRect(121, 8, 34, 8, BLACK);
-  StickCP2.Display.fillRect(119, 9, 38, 10, BLACK);
-
-  StickCP2.Display.setCursor(31, 6);
-  StickCP2.Display.printf("QUIK");
-  StickCP2.Display.setCursor(79, 6);
-  StickCP2.Display.printf("PROG");
-  StickCP2.Display.setCursor(127, 6);
-  StickCP2.Display.printf("GSET");
-  StickCP2.Display.setCursor(177, 6);
-  StickCP2.Display.printf("ABOUT");
-
-  StickCP2.Display.setCursor(35, 65);
-  StickCP2.Display.printf("Press M5 to enter this mode");  
 }
 
 void setAboutDisplay(){
-  StickCP2.Display.setTextSize(1);
-  StickCP2.Display.clear();
 
-  StickCP2.Display.drawFastHLine(0, 17, 240, GREEN);
-  StickCP2.Display.drawRect(169, 8, 45, 10, GREEN);
-  StickCP2.Display.fillRect(172, 8, 39, 8, BLACK);
-  StickCP2.Display.fillRect(170, 9, 43, 10, BLACK);
-
-  StickCP2.Display.setCursor(31, 6);
-  StickCP2.Display.printf("QUIK");
-  StickCP2.Display.setCursor(79, 6);
-  StickCP2.Display.printf("PROG");
-  StickCP2.Display.setCursor(127, 6);
-  StickCP2.Display.printf("GSET");
-  StickCP2.Display.setCursor(177, 6);
-  StickCP2.Display.printf("ABOUT");
-
-  StickCP2.Display.setCursor(35, 65);
-  StickCP2.Display.printf("Press M5 to enter this mode");  
 }
 
 //END FUNCTIONS
@@ -117,10 +62,12 @@ void setAboutDisplay(){
 void setup() {
     auto cfg = M5.config();
     StickCP2.begin(cfg);
-    StickCP2.Display.setRotation(3);
-    StickCP2.Display.setTextColor(GREEN);
 
-    setMainMenu();
+    StickCP2.Display.setRotation(1);
+    StickCP2.Display.setTextColor(WHITE);
+    StickCP2.Display.setTextFont(&fonts::FreeSerif9pt7b);
+
+    setMainMenu(2);
 }
 
 void loop() {
